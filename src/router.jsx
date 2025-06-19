221b1b5f9058a6b765dea27fcc19c73e812f6291
iratxeMoya <iratxe.moya@gmail.com>
2025-06-19 22:25:22 +0200

    routing funcionando

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,3 @@
-// src/router.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Servicios from "./pages/Servicios";
@@ -12,13 +11,13 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/servicios" element={<Servicios />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/sobremi" element={<SobreMi />} />
-          <Route path="/media" element={<Media />} />
-          <Route path="/contacto" element={<Contacto />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="servicios" element={<Servicios />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="sobremi" element={<SobreMi />} />
+          <Route path="media" element={<Media />} />
+          <Route path="contacto" element={<Contacto />} />
         </Route>
       </Routes>
     </BrowserRouter>
