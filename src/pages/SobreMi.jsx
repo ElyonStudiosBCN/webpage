202eb2a22e6aa015b8b476e29ea1c575c37438b2
iratxeMoya <iratxe.moya@gmail.com>
2025-06-23 13:25:22 +0200

    Sobre mi

--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -1,7 +1,27 @@
+// src/pages/SobreMi.jsx
+import "./SobreMi.css";
 import { useTranslation } from "react-i18next";
+import pau from "../assets/pau.jpg";
 
 export default function SobreMi() {
   const { t } = useTranslation();
-  
-    return <h1>{t("about.welcome")}</h1>;
+
+  return (
+    <section className="section section-light">
+      <div className="card">
+        <h1 className="heading text-primary">{t("about.welcome")}</h1>
+        <div className="aboutme-container">
+          <div>
+            <p className="text-muted text1">{t("about.description1.p1")}</p>
+            <p className="text-muted text1">{t("about.description1.p2")}</p>
+            <p className="text-muted text1">{t("about.description1.p3")}</p>
+          </div>
+          <img src={pau} alt="Pau Lefler" className="pau-image"/>
+        </div>
+        <div className="text2">
+          <p className="text-muted">{t("about.description2")}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
